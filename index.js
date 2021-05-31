c9f59d106f6bb92163520d90b139bf49ba8aefab
meir <meirankri@gmail.com>
2021-05-31 21:19:12 +0200

    every 10 min

--- index.js
+++ index.js
@@ -26,13 +26,30 @@ const press = botId => {
 
 const main = () => {
 	const date = new Date();
+	const dateFormated = format(date, 'H.m');
+	const hourAndMinNow = parseFloat(dateFormated);
 
-	press(bot1).then(() => {
-		setTimeout(() => press(bot2), 5000);
-	});
-	console.log(date);
+	if (isSunday(date)) {
+		const hourToOpen = satursdayHoursBetween.map(
+			hours => hours.startAt < hourAndMinNow && hours.endAt > hourAndMinNow,
+		);
+		if (hourToOpen.includes(true)) {
+			press(bot1).then(() => {
+				setTimeout(() => press(bot2), 5000);
+			});
+			console.log(date);
+		}
+	}
 };
 
-main();
+const satursdayHoursBetween = [
+	{ startAt: 9.45, endAt: 10.3 },
+	{ startAt: 13, endAt: 13.3 },
+	{ startAt: 14.3, endAt: 15.1 },
+	{ startAt: 16.4, endAt: 17 },
+	{ startAt: 18.3, endAt: 19.15 },
+];
 
+const satursdayHours = [10, 13, 14, 15, 16, 19, 20];
+setIntervalAsync(main, 600 * 1000);
 console.log(`is chaabes ${isSaturday(new Date())}`);
