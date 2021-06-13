96f244411dd9495dbbf8c2fb11b0936c2e060d44
meir <meirankri@gmail.com>
2021-06-13 07:48:44 +0200

    test with sunday 1min

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const satursdayHoursBetween = [
 ];
 
 const satursdayHours = [10, 13, 14, 15, 16, 19, 20];
-setIntervalAsync(main, 600 * 1000);
+setIntervalAsync(main, 60 * 1000);
 console.log(`is chaabes ${isSaturday(new Date())}`);
 console.log(token);
 
