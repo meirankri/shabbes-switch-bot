2d30d1100d647dc1b11743b38d09060127a26875
meir <meirankri@gmail.com>
2021-09-17 19:47:52 +0200

    change hour chabbat

--- index.js
+++ index.js
@@ -98,9 +98,8 @@ const satursdayHoursBetween = [
   { startAt: 9.45, endAt: 10.3 },
   { startAt: 10.5, endAt: 11.2 },
   { startAt: 12.2, endAt: 12.5 },
-  { startAt: 13, endAt: 13.3 },
-  { startAt: 14.3, endAt: 15.2 },
-  { startAt: 16.4, endAt: 17 },
+  { startAt: 14, endAt: 14.3 },
+  { startAt: 16.4, endAt: 17.15 },
   { startAt: 19, endAt: 19.45 },
   { startAt: 20.15, endAt: 21.0 },
 ];
